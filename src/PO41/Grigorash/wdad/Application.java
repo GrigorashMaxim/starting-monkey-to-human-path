adf29cf779c7b194cfca42afd1a1728724401579
GrigorashMaxim <maxim.grigorash123@gmail.com>
2017-01-17 20:49:09 +0400

    Added entry point

---
+++ src/PO41/Grigorash/wdad/Application.java
@@ -0,0 +1,12 @@
+package PO41.Grigorash.wdad;
+
+/**
+ * Created by MAXiM on 21.12.2016.
+ */
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I`m Maxim Grigorash, and I`m not a monkey");
+    }
+}
